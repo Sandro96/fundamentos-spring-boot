57a7c78e68ca198d3d9912ad1f7279142a3fbf16
Sandro Ramirez <sramirez@baufest.com>
2022-08-17 11:33:35 -0300

    Primer commit

---
+++ src/test/java/com/fundamentosplatzi/springboot/funtamentos/FuntamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fundamentosplatzi.springboot.funtamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuntamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
